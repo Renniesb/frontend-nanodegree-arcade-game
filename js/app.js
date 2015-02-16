d313e87afc31e3f20aae104f5be4cb16f577f49c
Rennie Bevineau <rsbevineau@gmail.com>
2015-02-16 19:01:14 +0100

    improve code quality

--- js/app.js
+++ js/app.js
@@ -9,7 +9,7 @@ var Enemy = function(x,y) {
 
     this.x=x;
     this.y=y;
-}
+};
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
@@ -21,19 +21,19 @@ Enemy.prototype.update = function(dt) {
 //conditions to advance bug 
 //and reset bug to other side of screen if it goes too far.
 
-    if(this.x<505){
+if(this.x<505){
    this.x+=(200*dt);
 }
 else{
     this.x=-100;
 } 
 
-}
+};
 
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-}
+};
 
 // Now write your own player class
 // This class requires an update(), render() and
@@ -41,22 +41,21 @@ Enemy.prototype.render = function() {
 
 
 
-    var Player=function(x,y){
-        this.sprite = 'images/char-boy.png';
-        this.x=x;
-        this.y=y;
+ var Player=function(x,y){
+    this.sprite = 'images/char-boy.png';
+    this.x=x;
+    this.y=y;
         
 
-    }
+};
 
 
-
-        Player.prototype.update = function(){
+Player.prototype.update = function(){
 
             
-            //Iterating for enemies with conditions that are dangerous to my player.
+     //Iterating for enemies with conditions that are dangerous to my player.
                    
-                  for(var enemy in allEnemies){
+          for(var enemy in allEnemies){
 
                      if(allEnemies[enemy].x+99 > this.x+16 && allEnemies[enemy].x <this.x+85 &&  allEnemies[enemy].y === this.y ) {
 
@@ -72,13 +71,13 @@ Enemy.prototype.render = function() {
                 player.reset();
             }
             
-        }
+        };
 
 
 
         Player.prototype.render = function(){
         ctx.drawImage(Resources.get(this.sprite),this.x,this.y);
-    } 
+    };
 
 
     //Set up if statements to respond to key presses 
@@ -105,7 +104,7 @@ Enemy.prototype.render = function() {
             }
             
 
-        }
+        };
 
         Player.prototype.reset= function(){
 
@@ -113,7 +112,7 @@ Enemy.prototype.render = function() {
 
             this.x=200;
             this.y=390;
-        }
+        };
 
 
 
@@ -121,12 +120,12 @@ Enemy.prototype.render = function() {
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 
-    enemy1 = new Enemy(-450,230);
-    enemy2 = new Enemy(-200,150);
-    enemy3 = new Enemy(-100,70);
+   var enemy1 = new Enemy(-450,230);
+   var enemy2 = new Enemy(-200,150);
+   var enemy3 = new Enemy(-100,70);
    
 
-    allEnemies=[enemy1,enemy2,enemy3];
+    var allEnemies=[enemy1,enemy2,enemy3];
 
 
 
